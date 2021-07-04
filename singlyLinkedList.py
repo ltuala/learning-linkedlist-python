1fa386872ffc67d70164d7d95359e9f6485848b0
Lyndon Tuala <lyndontuala28@gmail.com>
2021-07-04 19:55:42 +0800

    modify singlyLinkedList to insert a new node between two other nodes

--- singlyLinkedList.py
+++ singlyLinkedList.py
@@ -11,7 +11,34 @@ class LinkedList:
 		temporaryNode = self.head
 		self.head = newNode
 		self.head.next = temporaryNode
-		del temporaryNode 
+		del temporaryNode
+
+	def listlength(self):
+		currentNode = self.head
+		length = 0 
+		while currentNode.next != None:
+			length += 1
+			currentNode = currentNode.next
+		return length 
+
+	def insertAt(self, newNode, position):
+		if position < 0 or position > self.listlength():
+			print("Invalid position")
+			return
+		if position is 0:
+			self.insertHead(newNode)
+			return
+		currentNode = self.head
+		currentPosition = 0
+		while True:
+			if currentPosition == position:
+				previousNode.next = newNode
+				newNode.next = currentNode
+				break 
+			previousNode = currentNode 
+			currentNode = currentNode.next 
+			currentPosition += 1
+
 
 	def insertEnd(self, newNode):
 		if self.head is None:
@@ -35,12 +62,12 @@ class LinkedList:
 			print(currentNode.data)
 			currentNode = currentNode.next
 
-firstNode = Node("John")
+firstNode = Node(10)
 linkedList = LinkedList()
 linkedList.insertEnd(firstNode)
-secondNode = Node ("Ben")
+secondNode = Node (20)
 linkedList.insertEnd(secondNode)
-thirdNode = Node("Matthew")
-linkedList.insertHead(thirdNode)
+thirdNode = Node(15)
+linkedList.insertAt(thirdNode, 10)
 linkedList.printList()
 
