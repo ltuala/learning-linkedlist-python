1f7cf1483df6e8e8129a57cbf407640a0a5dbda9
Lyndon Tuala <lyndontuala28@gmail.com>
2021-07-04 09:02:23 +0800

    add a single linked list python file

---
+++ singlyLinkedList.py
@@ -0,0 +1,36 @@
+class Node:
+	def __init__(self,data);
+		self.data = data
+		self.next = None 
+
+class LinkedList:
+	def __init__(self);
+		self.head = None 
+
+	def insert(self, newNode);
+		if self.head is None:
+			self.head = newNode
+		else:
+			lastNode = self.head
+			while True:
+				if lastNode.next is None:
+					break 
+				lastNode = lastNode.next
+			lastNode.next = newNode
+
+	def printList(self):
+		currentNode = self.head 
+		while True:
+			if currentNode is None:
+				break
+			print(currentNode.data)
+			currentNode = currentNode.next
+firstNode = Node("John")
+linkedList = LinkedList()
+linkedList.insert(firstNode)
+secondNode = Node ("Ben")
+linkedList.insert(secondNode)
+thirdNode = Node("Matthew")
+linkedList.insert(thirdNode)
+linkedList.printList()
+
